0d9274b31844932a3dbe23faef80448a12fa53d2
chereddytriveni <chereddytriveni@gmail.com>
2025-10-29 17:34:56 +0530

    changed name for product

--- components_task/src/components/Productsection.jsx
+++ components_task/src/components/Productsection.jsx
@@ -1,7 +1,7 @@
 function Productsection(){ 
     return <div>
-        <h1>New product</h1> 
-        <h1>The Twist of Healthy Yogurt</h1> 
+        <h1>best selling product</h1> 
+        <h1>The Twist of Hhy Yogurt</h1> 
         <p>This website template has been designed by freewebsitetemplates.com for you, for free. You can replace all this text with your own text.</p>
         <img src="https://sample-website-pi.vercel.app/images/yogurt.jpg"/>
     </div>
