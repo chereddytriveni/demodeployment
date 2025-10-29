2e4cd1a7e3a927d72363c359c87b49285b3bb2a6
chereddytriveni <chereddytriveni@gmail.com>
2025-10-29 17:37:32 +0530

    changed import name

--- components_task/src/App.jsx
+++ components_task/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css' 
-import Navbar from "./components/Navbar" 
+import Navbar from "./components/navbar" 
 import Bannersection from "./components/Bannersection" 
 import Productsection from "./components/Productsection" 
 import Exploreussection from "./components/Exploreussection" 
